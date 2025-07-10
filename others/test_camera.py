24bdd8c7709513abd6d191a5ac392fa3cea74376
thinkercom <150505802+thinkercom@users.noreply.github.com>
2025-07-10 14:33:27 +0800

    其他代码，包含摄像头测试等

---
+++ others/test_camera.py
@@ -0,0 +1,13 @@
+import cv2
+cap0=cv2.VideoCapture(2)
+try:
+    while True:
+        ret,frame0=cap0.read()
+        if ret:
+            print("success")
+        else:
+            print("failed")
+        cv2.waitKey(1)
+except:
+    print("end")
+    cap0.release()
